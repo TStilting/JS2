98909d7015f4cbdf97af2ffa8fcc0dc0a4cb96d4
TStilting <tstilting@roc-dev.com>
2020-12-14 12:47:14 +0100

    project aangemaakt

---
+++ H2/JS4.js
@@ -0,0 +1,4 @@
+document.getElementById("header").innerHTML = "Dit is de titelpagina";
+document.getElementById("sub").innerHTML = "Dit is de subkop";
+document.getElementById("foto1").src = "BellenBlazen3.jpg";
+document.getElementById("foto2").src = "BellenBlazen4.jpg";
---
+++ H2/JS5.js
@@ -0,0 +1,14 @@
+document.getElementById("B1").style.backgroundColor = "cyan";
+document.getElementById("B2").style.backgroundColor = "green";
+document.getElementById("B3").style.backgroundColor = "yellow";
+document.getElementById("B4").style.backgroundColor = "red";
+
+document.getElementById("B1").style.textAlign = "center";
+document.getElementById("B2").style.textAlign = "center";
+document.getElementById("B3").style.textAlign = "right";
+document.getElementById("B4").style.textAlign = "left";
+
+document.getElementById("B2").style.marginTop = "20px";
+document.getElementById("B3").style.marginTop = "40px";
+
+
