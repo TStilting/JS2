e9d0f11b5090e255a6644ddbfc507c7c1bba25fa
Troy <tstilting@roc-dev.com>
2021-01-07 11:21:21 +0100

    H4 verder gewerkt

--- H4/JS8.js
+++ H4/JS8.js
@@ -1,3 +1,8 @@
 var fotos = [1,2,3];
 var placeholder = document.getElementById("placeholder");
-placeholder.style.backgroundImage = "url('img/foto1.jpg')";
+
+placeholder.style.backgroundImage = "url('Foto1.jpg')";
+
+placeholder.addEventListener("click" ,function() {
+    placeholder.style.backgroundImage = "url('Foto2.jpg')";
+})
