1f21cbed87e6e378fdbcece444b072a0d1360815
Troy <tstilting@roc-dev.com>
2021-01-08 10:05:16 +0100

    H4 nog meer verder gewerkt

--- H4/JS8.js
+++ H4/JS8.js
@@ -1,8 +1,17 @@
 var fotos = [1,2,3];
+var teller = 1;
 var placeholder = document.getElementById("placeholder");
 
 placeholder.style.backgroundImage = "url('Foto1.jpg')";
 
 placeholder.addEventListener("click" ,function() {
-    placeholder.style.backgroundImage = "url('Foto2.jpg')";
+    placeholder.style.backgroundImage = "url('Foto" + rotatie() + ".jpg')";
 })
+
+function rotatie() {
+    teller++;
+    if (teller >= 4) {
+        teller = 1;
+    }
+    return teller;
+}
