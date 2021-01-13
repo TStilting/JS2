e2f56e8823f2ab103269127739b8f527800496bd
Troy <tstilting@roc-dev.com>
2021-01-13 10:17:10 +0100

    meer fotos toegevoegd

--- H5/JS10.js
+++ H5/JS10.js
@@ -11,11 +11,26 @@ function createFotoholder () {
     }
 }
 
+
 function getImage() {
     image = document.getElementsByClassName("fotoo");
     for (var i = 0; i < 9; i++) {
+        fav = document.createElement("div");
+        fav.className = "ster";
+        fav.id = "fav" + (i+1);
         imagee = document.createElement("img");
         imagee.src = "Turtle" + (i+1) + ".jpg";
+        imagee.id = (i+1);
+        imagee.addEventListener("click", function(){
+            addSter(this.id);
+        });
         image[i].appendChild(imagee);
+        image[i].appendChild(fav);
     }
+}
+
+
+function addSter(id) {
+    fav = document.getElementById("fav" + id);
+    fav.style.backgroundImage ("Ster.png");
 }
